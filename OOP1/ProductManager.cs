905a839e1466a95f2c77a8d808f08f234cc45727
nurcantemur1 <nurtemur12@gmail.com>
2023-05-17 11:44:30 +0300

    oop1

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product) 
+        {
+            Console.WriteLine(product.ProductName);
+            
+        }
+        public void Update() 
+        {
+            Console.WriteLine("guncellendi");
+        }
+        public void Delete() 
+        {
+
+        }
+      
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Product product = new Product();
+            product.ProductId = 1;
+            product.CategoryId = 1;
+            product.ProductName= "hh";
+            product.UnitPrice = 10;
+            product.UnitInStock = 3;
+
+            
+          //  Console.WriteLine(product.ProductName);
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
