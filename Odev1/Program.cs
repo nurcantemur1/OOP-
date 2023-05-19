fa392ad29524c224d8dab5a2c1e60f8552d2229f
nurcantemur1 <nurtemur12@gmail.com>
2023-05-20 00:06:19 +0300

    odev1

---
+++ Odev1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gamer gamer = new Gamer();
+            Console.WriteLine("Adınız :");
+            gamer.Ad = Console.ReadLine();
+            Console.WriteLine("Soyad :");
+            gamer.Soyad = Console.ReadLine();
+            Console.WriteLine("DogumYili :");
+            gamer.DogumYili= Console.ReadLine();
+            Console.WriteLine("TcNo :");
+            gamer.TcNo= Console.ReadLine(); 
+
+            GamerManager gamerManager = new GamerManager();
+            gamerManager.Add(gamer);
+            gamerManager.Update(gamer);
+            gamerManager.Delete(gamer);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
